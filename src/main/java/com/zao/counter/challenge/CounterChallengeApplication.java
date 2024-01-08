c9367134c85906770195daaafa818ef158ae5d60
ozoomosigho <ozo.omosigho@sainsburys.co.uk>
2024-01-08 23:44:09 +0000

    Created Maven Project for Counter Challenge

---
+++ src/main/java/com/zao/counter/challenge/CounterChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.zao.counter.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounterChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CounterChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zao/counter/challenge/CounterChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zao.counter.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
